cbdd2e42c234c5eabf96d7a141bda3eda60a20bc
rodrigo kalil <rkbuarque@gmail.com>
2021-09-24 15:57:54 -0300

    listar extrato bancario por data

--- src/index.js
+++ src/index.js
@@ -102,5 +102,16 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) =>{
     return response.status(201).send();
 });
 
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => { // middleware usado dessa forma se eu quiser que apenas algumas rotas especificas usem
+    const { customer } = request; 
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter((statement) => statement.created_at.toDateString() === new Date(dateFormat).toDateString());
+
+    return response.json(statement);
+});
+
 // localhost:3333
 app.listen(3333);

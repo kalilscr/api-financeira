a256ff936b640f97828757d0f84b3b14351faf9b
rodrigo kalil <rkbuarque@gmail.com>
2021-09-24 16:29:53 -0300

    atualizar e remover conta, e retornar o balanço

--- src/index.js
+++ src/index.js
@@ -113,5 +113,37 @@ app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => { //
     return response.json(statement);
 });
 
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+});
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    return response.json(customer);
+});
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+});
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+});
+
+
 // localhost:3333
 app.listen(3333);
